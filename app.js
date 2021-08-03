e11e8c356d63624f6d45a5c12d57b5e3bcbe406c
Tharunraj Optimus <tharunoptimus@outlook.com>
2021-08-04 03:04:04 +0530

    Full Functionality Done. Styling remaining

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 // Set constraints for the video stream
-var constraints = { video: { facingMode: "user" }, audio: false };
-var track = null;
+let constraints = { video: { facingMode: "user" }, audio: false };
+let track = null;
+
+let showingResults = false;
 
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger");
+    cameraTrigger = document.querySelector("#camera--trigger")
+    predictionSpan = document.querySelector("#prediction")
+    currentTaskImage = document.querySelector("#currentTask");
 
 // Access the device camera and stream to cameraView
 function cameraStart() {
@@ -23,26 +27,74 @@ function cameraStart() {
 
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = async function() {
+
+  if(!showingResults) {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
 
-    // Predict the imaage with the model
+    applyStyleAndShowStatus(true);
     let prediction = await predictModal();
-    showResults(prediction);
-    // track.stop();
+    predictionSpan.innerHTML = showResults(prediction);
+    changeCurrentTask(false);
+    showingResults = true;
+  }
+  else {
+    applyStyleAndShowStatus(false);
+    removePhotoFromFrame();
+    changeCurrentTask(true);
+    showingResults = false;
+  }
+    
+
 };
 
+function applyStyleAndShowStatus(show) {
+  if(show) {
+    // add class to the predictionSpan element without jquery
+    predictionSpan.classList.add("prediction");
+    predictionSpan.innerHTML = "<img src='searching.gif' alt='Scanning'>";
+  }
+  else {
+    predictionSpan.classList.remove("prediction");
+    predictionSpan.innerHTML = "";
+  }
+}
+
+function removePhotoFromFrame () {
+  cameraOutput.classList.remove("taken");
+  cameraOutput.src = "";
+}
+
+function changeCurrentTask (ready) {
+  if(ready) {
+    currentTaskImage.src = "search.png";
+  }
+  else {
+    currentTaskImage.src = "cancel.png";
+  }
+}
+
 function showResults(props) {
-    let html = "<ul>"
+    let html = "<div style='margin-bottom: 0.5rem;'>Results:</div><ul class='predictions'>"
     props.forEach(element => {
-      html  += "<li>" + element.className + ": " + element.probability + "</li>";
+      html  += "<li>" + 
+                  "<div class='predictionDiv'>" + element.className + "</div>"
+                  + "<div class='predictionPercentage'>" + getPercentage(parseFloat(element.probability)) + "</div>" 
+              + "</li>";
     });
     html += "</ul>";
-    document.getElementById("prediction").innerHTML = html;
+    return html;
 }
+
+
+// function to conver the string input 0.5909343 to a percentage
+function getPercentage (number) {
+    return (number * 100).toFixed(2) + "%";
+}
+
 
 
 async function predictModal () {
