964a83a8129016a605a21bbd04a869461b5f0ce1
Tharunraj Optimus <tharunoptimus@outlook.com>
2021-08-04 04:30:58 +0530

    PWA Ready

--- js/app.js
+++ js/app.js
@@ -133,3 +133,12 @@ async function predictModal () {
 }
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
+
+
+if('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('serviceworker.js')
+      .then((reg) => console.log('Success: ', reg.scope))
+      .catch((err) => console.log('Failure: ', err));
+  })
+}
