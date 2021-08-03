ca3fe4a5535911f5b5e4218132425d9fe4b3409d
Tharunraj Optimus <tharunoptimus@outlook.com>
2021-08-04 02:21:38 +0530

    Core working

--- app.js
+++ app.js
@@ -22,14 +22,42 @@ function cameraStart() {
 }
 
 // Take a picture when cameraTrigger is tapped
-cameraTrigger.onclick = function() {
+cameraTrigger.onclick = async function() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
+
+    // Predict the imaage with the model
+    let prediction = await predictModal();
+    showResults(prediction);
     // track.stop();
 };
 
+function showResults(props) {
+    let html = "<ul>"
+    props.forEach(element => {
+      html  += "<li>" + element.className + ": " + element.probability + "</li>";
+    });
+    html += "</ul>";
+    document.getElementById("prediction").innerHTML = html;
+}
+
+
+async function predictModal () {
+  let prediction;
+  let img = document.getElementById('camera--output');
+
+// Load the model.
+  await mobilenet.load().then(async model => {
+  // Classify the image.
+      await model.classify(img).then(predictions => {
+          prediction = predictions;
+      });
+  });
+
+  return prediction;
+}
 // Start the video stream when the window loads
 window.addEventListener("load", cameraStart, false);
